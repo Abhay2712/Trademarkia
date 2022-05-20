3221bbd0e24c2434a08f0f761d7f68a121e7d4ea
Abhay2712 <abhaysharma754@gmail.com>
2022-05-20 14:05:06 +0530

    database setup

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+func CreatePersonEndpoint(response http.ResponseWriter, request *http.Request) {}
+func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request)    {}
+func GetPersonEndpoint(response http.ResponseWriter, request *http.Request)    {}
 
 func main() {
-	name := "Go Developers"
-	fmt.Println("Azure for", name)
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/person", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/person/{id}", GetPersonEndpoint).Methods("GET")
+	http.ListenAndServe(":12345", router)
 }
